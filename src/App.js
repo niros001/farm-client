64fd9bef5ace4053c7f7331420d568dbff294f4c
niros <nirh@welldone-software.com>
2021-02-22 11:07:10 +0200

    Filter by relevant fields Fix CSV export button

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {useMemo, useState, useCallback} from 'react'
 import styled from 'styled-components';
 import {compact} from 'lodash'
 import {notification, Table, Popover, Button, Modal} from 'antd';
-import {CSVReader} from 'react-papaparse'
+import {CSVReader, CSVDownloader} from 'react-papaparse'
 
 const Container = styled.div`
   display: flex;
@@ -29,8 +29,9 @@ const ItemsRenderer = ({items}) => {
 const App = () => {
     const [data, setData] = useState([]);
     const [visible, setVisible] = useState(false);
-    const columns = useMemo(() => compact(data[0]).map((title, index) => ({title, dataIndex: index, key: index})), [data]);
-    const dataSource = useMemo(() => compact(data.slice(1, data.length)).map((d) => d.map((items, i) => ((i === 16) || (i === 23)) ? (<Popover content={<ItemsRenderer items={items} />}><Button>Items</Button></Popover>) : items)), [data]);
+    const relevantFields = useMemo(() => [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25], []);
+    const columns = useMemo(() => compact(data[0]).filter((_, i) => relevantFields.includes(i)).map((title, index) => ({title, dataIndex: index, key: index})), [relevantFields, data]);
+    const dataSource = useMemo(() => compact(data.slice(1, data.length)).map((d) => d.filter((_, i) => relevantFields.includes(i)).map((items, i) => ((i === 16) || (i === 23)) ? (<Popover content={<ItemsRenderer items={items} />}><Button>Items</Button></Popover>) : items)), [relevantFields, data]);
 
     const handleOnDrop = useCallback((data) => {
         setData(data.map(({data}, index) => [...data, index === 0 ? 'פריטים מעודכנים' : '']));
@@ -63,7 +64,13 @@ const App = () => {
                     onClick: () => setVisible(true)
                 })}
             />
-            <Button style={{alignSelf: 'center', margin: 20}} type="primary">Export to CSV/XLS</Button>
+            <CSVDownloader
+                data={data}
+                filename={'updated_csv'}
+                type={'link'}
+            >
+                <Button style={{margin: 20}} type="primary">Export to CSV/XLS</Button>
+            </CSVDownloader>
             <Modal title="Update Items" visible={visible} onCancel={() => (setVisible(false))} footer={null}>
                 <p>Update items...</p>
             </Modal>
